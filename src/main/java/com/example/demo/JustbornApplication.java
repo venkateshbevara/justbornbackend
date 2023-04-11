fb84301a642692d1408c7a2b4540fc11cf12a875
venkateshbevara <sachinvenkyindian@gmail.com>
2023-04-11 16:44:55 +0530

    justbornbackend

---
+++ src/main/java/com/example/demo/JustbornApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustbornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustbornApplication.class, args);
+	}
+
+}
